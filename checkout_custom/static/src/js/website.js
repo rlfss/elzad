1f67429aae6167ee3f07eeb5aa6465ba51e7f469
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-03-25 21:41:00 +0200

    Add files via upload

---
+++ checkout_custom/static/src/js/website.js
@@ -0,0 +1,34 @@
+
+odoo.define('checkout_custom.website', function (require) {
+"use strict";
+    var base = require('web_editor.base');
+    var ajax = require('web.ajax');    
+       
+    $('.oe_website_sale').each(function () {
+        var oe_website_sale = this;
+        $(oe_website_sale).on('click', '.btn.btn-primary.pull-right.mb32', function (ev)
+        {
+            var conf_value = $(this).find('.wk_cart_values').attr('conf_value');
+            var cart_value = $('#order_total>:last>span>span').text();
+            var cart_value = cart_value.replace(",", "");
+            var cart_value = parseFloat(cart_value);
+            var currency_symbol = $(this).find('.wk_cart_values').attr('currency_symbol');
+            var $link = $(this);
+            setTimeout(function() {$link.popover('destroy')},3000);
+            if (cart_value < conf_value)
+            {
+                ev.preventDefault(); 
+                $(this).popover({
+                  content:"A minimum purchase total of "+ currency_symbol+" "+ conf_value+" is required to validate your order, current purchase total is "+ currency_symbol+" "+ cart_value,
+                  title:"WARNING",
+                  placement:"top",
+                  trigger:'focus',
+                });
+                $(this).popover('show');
+            }
+        });
+
+
+       
+	});
+});
