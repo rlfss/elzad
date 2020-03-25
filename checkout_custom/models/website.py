903d3f5775468f55c5c105e95900cbd45f1bee5a
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-03-25 02:35:10 +0200

    Update website.py

--- checkout_custom/models/website.py
+++ checkout_custom/models/website.py
@@ -7,3 +7,30 @@ from odoo.exceptions import UserError
 
 class website(models.Model):
     _inherit = 'website'
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+    
+    city_sel = fields.Many2one('res.partner.city', string='City')
+    zone = fields.Many2one('res.partner.zone', string='Zone')
+    
+    @api.onchange('city_sel')
+    def _onchange_city_sel(self):
+        if self.city_sel:
+            self.city = self.city_sel.name
+
+    
+class CitySelect(models.Model):
+    _name = 'res.partner.city'
+    _description = 'City Select'
+    
+    name =  fields.Char('Name')
+    code = fields.Char('Code')
+    
+class ZoneSelect(models.Model):
+    _name = 'res.partner.zone'
+    _description = 'Zone Select'
+    
+    name =  fields.Char('Name')
+    city_sel = fields.Many2one('res.partner.city', string='City')
+    code = fields.Char('Code')
