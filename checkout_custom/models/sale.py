a9ac7f7688b2097c76b8535352d1ba4f29e3c52d
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-03-26 07:09:49 +0200

    Update sale.py

--- checkout_custom/models/sale.py
+++ checkout_custom/models/sale.py
@@ -17,25 +17,26 @@ class SaleOrder(models.Model):
     customer_zone = fields.Many2one(related='partner_id.zone', string='Zone', readonly=True)
     sales_agent = fields.Many2one('sale.order.agent', string='Sales Agent')
 
-    def get_minimun_cart(self):
-        
-        ir_default = self.env['website'].get_current_website().c_id._convert(
+	def get_minimun_cart_value(self):
+		
+		ir_default = self.env['website'].get_current_website().c_id._convert(
                     self.env['website'].get_current_website().minimum_order_value, request.env['website'].get_current_website().pricelist_id.currency_id, self.env.user.company_id,
                     fields.Date.today()
                     )
-        return 1 if ir_default == None else round(ir_default, 2)
-    @api.model
-    def _get_errors(self, order):
-        
-        minimum_order_value =1 if self.env['website'].sudo().get_current_website().minimum_order_value == None else self.env['website'].sudo().get_current_website().c_id._convert(
+		
+		return 1 if ir_default == None else round(ir_default, 2)
+	@api.model
+	def _get_errors(self, order):
+		
+		minimum_order_value =1 if self.env['website'].sudo().get_current_website().minimum_order_value == None else self.env['website'].sudo().get_current_website().c_id._convert(
                     self.env['website'].sudo().get_current_website().minimum_order_value, self.env['website'].sudo().get_current_pricelist().currency_id, self.env.user.company_id,
                     fields.Date.today()
                     )
-        minimum_order_value = round(minimum_order_value,2)
-        errors = []
-        if order and order.amount_total < minimum_order_value:
-            errors.append(['Invalid Cart Value',"A minimum purchase is %s%s , current purchase total is %s%s "%(order.currency_id.symbol ,minimum_order_value, order.currency_id.symbol ,order.amount_total)])
-        return errors
+		minimum_order_value = round(minimum_order_value,2)
+		errors = []
+		if order and order.amount_total < minimum_order_value:
+			errors.append(['Invalid Cart Value',"A minimum purchase total of %s%s is required to confirm your order, current purchase total is %s%s "%(order.currency_id.symbol ,minimum_order_value, order.currency_id.symbol ,order.amount_total)])
+		return errors
 
 
 class SaleOrderAgent(models.Model):
@@ -58,5 +59,5 @@ class SaleOrderAgent(models.Model):
 class website(models.Model):
     _inherit = 'website'
 
-    minimum_order_value = fields.Float(string='Minimum Cart Value', digits=dp.get_precision('Product Price'))
+    minimum_order_value = fields.Float(string = 'Minimum Cart Value To Validate Order')
     c_id = fields.Many2one('res.currency', 'Currency',default=lambda self: self.env.user.company_id.currency_id.id,required=True)
