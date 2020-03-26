4c0679a0e0a0cbcec3573e49559e2d43f6aff826
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-03-26 07:24:59 +0200

    Update controller.py

--- checkout_custom/controllers/controller.py
+++ checkout_custom/controllers/controller.py
@@ -114,18 +114,18 @@ class WebsiteSale(WebsiteSale):
         }
         return request.render("website_sale.address", render_values)
 
-	@http.route(["/website/min_lang"], type='json', auth="public", methods=['POST'], website=True)
-	def website_langauge(self, code, **kw):
-		lang_id = request.env['res.lang'].search([('code','=',code.replace('-','_'))])
-		return {
-			'sep_format': lang_id.grouping,
-			'decimal_point': lang_id.decimal_point,
-			'thousands_sep': lang_id.thousands_sep
-		}
+    @http.route(["/website/min_lang"], type='json', auth="public", methods=['POST'], website=True)
+    def website_langauge(self, code, **kw):
+        lang_id = request.env['res.lang'].search([('code','=',code.replace('-','_'))])
+        return {
+            'sep_format': lang_id.grouping,
+            'decimal_point': lang_id.decimal_point,
+            'thousands_sep': lang_id.thousands_sep
+        }
 
 
-	def checkout_redirection(self, order):
-		minimum_order_value = 1 if not request.website.minimum_order_value else request.website.minimum_order_value
-		if  minimum_order_value and order.amount_total < minimum_order_value:
-			return request.redirect('/shop/cart')
-		return super(website_sale, self).checkout_redirection(order)
+    def checkout_redirection(self, order):
+        minimum_order_value = 1 if not request.website.minimum_order_value else request.website.minimum_order_value
+        if  minimum_order_value and order.amount_total < minimum_order_value:
+            return request.redirect('/shop/cart')
+        return super(WebsiteSale, self).checkout_redirection(order)
