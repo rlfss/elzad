2f17f46b2427c7ad9a402b850b6821607286b9a0
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-04-05 22:22:44 +0300

    Update sale_custom_line.py

--- sale_custom_line/models/sale_custom_line.py
+++ sale_custom_line/models/sale_custom_line.py
@@ -9,7 +9,7 @@ class SaleOrderLine(models.Model):
     _name = 'sale.order.line'
     _inherit = 'sale.order.line'
 
-    categ_id = fields.Many2one('product.category',related='product_id.category_id', string='Product Category', readonly=True)
+    categ_id = fields.Many2one('product.category',related='product_id.categ_id', string='Product Category', readonly=True)
 
     @api.depends('order_id.order_line')
     def _compute_get_number(self):
