e80cdd7f38e0d252592c32c5b61dbaedc9365635
sahara-yeh <55381730+sahara-yeh@users.noreply.github.com>
2020-06-21 11:51:40 +0300

    module:website_multi_region upload "TEST"

---
+++ website_multi_region/controllers/main.py
@@ -0,0 +1,12 @@
+from odoo import http
+from odoo.http import request
+
+class Regoins(http.Controller):
+    @http.route(['''/shop/change_pricelist<model('website.branches'):br>'''], type='http', auth="public", website=True)
+    def pricelist_change(self,br,**post):
+        pl = br
+        if (pl.selectable or pl == request.env.user.partner_id.property_product_pricelist) \
+                and request.website.is_pricelist_available(pl.id):
+            request.session['website_sale_current_pl'] = pl
+            request.website.sale_get_order(force_pricelist=pl)
+        return request.redirect(request.httprequest.referrer or '/shop')
---
+++ website_multi_region/models/regions.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class Regions(models.Model):
+    _name = 'website.branches'
+
+    name = fields.Char("Name")
+    code = fields.Char("Code")
+    stock_location = fields.Many2one('stock.location',String = "Stock Location")
+    sales_team = fields.Many2one('crm.team',String = "Sales Team")
+    pricelist = fields.Many2one('product.pricelist',String = "Price")
+
+    def get_branches_available(self):
+        return self.search([])
+
