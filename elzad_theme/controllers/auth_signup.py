56a074fcc42383084f619e5cafbf70d8087d812a
mohamed.derbel <mohamed.derbel@fogits.com>
2020-07-19 17:08:36 +0100

    optimization form sign up

--- elzad_theme/controllers/auth_signup.py
+++ elzad_theme/controllers/auth_signup.py
@@ -11,9 +11,9 @@ class AuthSignupHomeExt(AuthSignupHome):
     def do_signup(self, qcontext):
         """ Replaced the default do_signup base module to add extra fields. """
         if qcontext.get('company_type') == 'company':
-            values = {key: qcontext.get(key) for key in ('login', 'password', 'phone', 'name', 'vat','company_type')}
+            values = {key: qcontext.get(key) for key in ('login', 'password', 'phone', 'name','company_type')}
         if qcontext.get('company_type') == 'person':
-            values = {key: qcontext.get(key) for key in ('login', 'password', 'phone', 'date_of_birth', 'sex','company_type')}
+            values = {key: qcontext.get(key) for key in ('login', 'password', 'phone', 'date_of_birth', 'sex','company_type','title')}
             values['name'] = qcontext.get('first_name') + ' ' + qcontext.get('last_name')
         if not values:
             raise UserError(_("The form was not properly filled in."))
@@ -22,5 +22,6 @@ class AuthSignupHomeExt(AuthSignupHome):
         supported_langs = [lang['code'] for lang in request.env['res.lang'].sudo().search_read([], ['code'])]
         if request.lang in supported_langs:
             values['lang'] = request.lang
+        print('values=>',values)
         self._signup_with_values(qcontext.get('token'), values)
         request.env.cr.commit()
